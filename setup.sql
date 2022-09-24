e8dfe98ea28b20cde3cbbc3c0abf6152a804ee12
Annika <nikzneko@gmail.com>
2022-09-24 09:23:34 -0700

    rebuild transactions table

--- setup.sql
+++ setup.sql
@@ -1,6 +1,7 @@
 -- Active: 1663625309675@@147.182.242.232@3306@budgit
 CREATE TABLE IF NOT EXISTS transactions(
-  id INT NOT NULL PRIMARY KEY COMMENT 'transactionId',
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'transactionId',
+  userId VARCHAR(255) NOT NULL COMMENT 'userIf this transaction belongs to',
   bankAccountId INT COMMENT 'bankAccountId this transaction is in',
   categoryId INT COMMENT 'categoryId this transaction is in',
   datePosted DATE NOT NULL COMMENT 'DATE this transaction was posted',
@@ -9,11 +10,7 @@ CREATE TABLE IF NOT EXISTS transactions(
   description VARCHAR(255) COMMENT 'comments/notes/description'
 ) DEFAULT CHARSET utf8;
 
-ALTER TABLE transactions
--- MODIFY COLUMN id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'transactionId';
-ADD userId VARCHAR(255) NOT NULL COMMENT 'userId this transaction belongs to';
-
 CREATE TABLE IF NOT EXISTS categories(
-  if NOT NULL PRIMARY KEY COMMENT 'categoryId',
-
+  id INT NOT NULL PRIMARY KEY COMMENT 'categoryId',
+  name 
 )
