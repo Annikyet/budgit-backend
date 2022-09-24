94f3b0648fd39de01eab8c856ecac215bd404545
Annika <nikzneko@gmail.com>
2022-09-24 09:18:49 -0700

    add userId to transaction

--- setup.sql
+++ setup.sql
@@ -1,3 +1,4 @@
+-- Active: 1663625309675@@147.182.242.232@3306@budgit
 CREATE TABLE IF NOT EXISTS transactions(
   id INT NOT NULL PRIMARY KEY COMMENT 'transactionId',
   bankAccountId INT COMMENT 'bankAccountId this transaction is in',
@@ -7,3 +8,12 @@ CREATE TABLE IF NOT EXISTS transactions(
   payee VARCHAR(255) COMMENT 'entity this transaction is with',
   description VARCHAR(255) COMMENT 'comments/notes/description'
 ) DEFAULT CHARSET utf8;
+
+ALTER TABLE transactions
+-- MODIFY COLUMN id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'transactionId';
+ADD userId VARCHAR(255) NOT NULL COMMENT 'userId this transaction belongs to';
+
+CREATE TABLE IF NOT EXISTS categories(
+  if NOT NULL PRIMARY KEY COMMENT 'categoryId',
+
+)
