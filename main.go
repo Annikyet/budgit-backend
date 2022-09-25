ddea906c0ac3fb0b23c4af378c48ff94a8ba783a
Annika <nikzneko@gmail.com>
2022-09-24 17:36:43 -0700

    created transaction model

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+// import (
+// 	"net/http"
+// 	"github.com/gin-gonic/gin"
+// 	"errors"
+// )
+
+type transaction struct {
+	ID            int
+	UserId        string
+	BankAccountId int
+	CategoryId    int
+	Date          string // should be a date type... later
+	Amount        int    // should be a decimal type... later
+	Payee         string
+	Description   string
+}
